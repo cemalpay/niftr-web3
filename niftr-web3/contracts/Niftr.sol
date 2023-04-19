9415c9d6429d79202cb1967bd5b76a595bd93370
cemalpay <cem.alpay.tas@common.studio>
2023-04-19 14:42:30 +0300

    create: getDonators function

--- niftr-web3/contracts/Niftr.sol
+++ niftr-web3/contracts/Niftr.sol
@@ -51,7 +51,11 @@ contract Niftr {
         }
     }
 
-    function getDonators(){}
+    function getDonators(uint256 _id) view public returns(address[] memory, uint256[] memory){
+        return (campaigns[_id].donators, campaigns[_id].donations);
+
+
+    }
 
     function getCampaigns(){}
 }
