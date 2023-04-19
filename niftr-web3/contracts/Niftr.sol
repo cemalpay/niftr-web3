e0bd3f8272c6eb20b9be4db77032500928f1bbb0
cemalpay <cem.alpay.tas@common.studio>
2023-04-19 14:25:19 +0300

    create: newCampaign function

--- niftr-web3/contracts/Niftr.sol
+++ niftr-web3/contracts/Niftr.sol
@@ -17,7 +17,25 @@ contract Niftr {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256){}
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256){
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        //is everything ok?
+        require(campaign.deadline < block.timestamp, "Campaign deadline has passed"); 
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+
+    }
 
     function donateToCampaign(){}
 
