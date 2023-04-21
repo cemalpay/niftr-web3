d2706140006beb893a4e4741f6db86024e2d8fd4
cemalpay <cem.alpay.tas@common.studio>
2023-04-21 19:10:22 +0300

    Update App.jsx

--- Niftr/src/App.jsx
+++ Niftr/src/App.jsx
@@ -4,7 +4,16 @@ import MainMint from "./MainMint";
 import NavBar from "./NavBar";
 
 function App() {
-  return <></>;
+  const [accounts, setAccounts] = useState([]);
+
+  return (
+    <>
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+    </>
+  );
 }
 
 export default App;
