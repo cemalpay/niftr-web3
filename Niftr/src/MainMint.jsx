cbff3f623e91936eedc2c385d72e92de6914db7a
cemalpay <cem.alpay.tas@common.studio>
2023-04-21 20:07:06 +0300

    create handleMint function

--- Niftr/src/MainMint.jsx
+++ Niftr/src/MainMint.jsx
@@ -6,4 +6,17 @@ const niftrNFTAddress = "0xD1D54342d992c7fe54bdCa09A8270C24E09547cF";
 
 const MainMint = ({ accounts, setAccounts }) => {
   const [mintAmount, setMintAmount] = useState(1);
+  const isConnected = Boolean(accounts[0]);
+
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        niftrNFTAddress,
+        NiftrNFT.abi,
+        signer
+      );
+    }
+  }
 };
