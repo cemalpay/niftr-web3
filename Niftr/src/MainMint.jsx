02be078933070d55ea7f3ddff2983caa6212fefd
cemalpay <cem.alpay.tas@common.studio>
2023-04-21 19:49:02 +0300

    Create MainMint.jsx
    import req plugins
    import niftrNFT.json
    add contract adress as a variable

---
+++ Niftr/src/MainMint.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { ethers, BigNumber } from "ethers";
+import NiftrNFT from "./NiftrNFT.json";
+
+const niftrNFTAddress = "0xD1D54342d992c7fe54bdCa09A8270C24E09547cF";
+
+const MainMint = ({ accounts, setAccounts }) => {
+  const [mintAmount, setMintAmount] = useState(1);
+};
